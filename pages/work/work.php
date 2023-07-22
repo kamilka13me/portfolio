a0d4146c75567f6a7eab9db1f96b73e6f85c516f
kamilka13me <kamilka13me@gmail.com>
2023-07-22 14:59:25 +0300

    work page first tab

--- pages/work/work.php
+++ pages/work/work.php
@@ -1,4 +1,5 @@
 <div class="work_page">
+
     <div class="header">
         <div class="left_side">
             <div class="left_side_container">
@@ -18,6 +19,50 @@
     </div>
 
     <div class="first_tab">
+        
+        <!-- background fradient -->
+        <div class="gradiend">
+            <svg xmlns="http://www.w3.org/2000/svg" width="475" height="1092" viewBox="0 0 475 1092" fill="none">
+                <g filter="url(#filter0_f_46_65)">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M-382.905 496.386C-355.406 340.691 -229.088 214.912 -71.7814 201.283C72.5351 188.78 180.343 309.79 238.637 442.536C292.803 565.884 290.721 712.175 193.54 805.472C91.7204 903.223 -62.1838 917.302 -186.473 850.659C-317.944 780.165 -408.884 643.482 -382.905 496.386Z"
+                        fill="url(#paint0_linear_46_65)" />
+                </g>
+                <defs>
+                    <filter id="filter0_f_46_65" x="-587.345" y="0.392212" width="1061.59" height="1091.33"
+                        filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                        <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_46_65" />
+                    </filter>
+                    <linearGradient id="paint0_linear_46_65" x1="-379.696" y1="479.733" x2="279.551" y2="609.616"
+                        gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#FC50B7" />
+                        <stop offset="1" stop-color="#FFC56F" stop-opacity="0.46" />
+                    </linearGradient>
+                </defs>
+            </svg>
+        </div>
+
+        <!-- first tab content -->
+        <div class="first_tab_container">
+            <div class="tag_button">tag</div>
+            <h1>about me</h1>
+            <h2>You can add what outcomes has this project brought after your design! For example, can show some real data.</h2>
+            <div class="contact_btn">tell me more</div>
+            
+        </div>
+    </div>
+
+
+
+    <div class="second_tab">
+        <div class="second_tab_container">
+
+        </div>
+
+    </div>
+    <div class="third_tab">
 
     </div>
 </div>
